7f54b5ddf1aabffd1a657206af1aebe25f0b23d3
AlexAlonsoMontero <lxalonso@gmail.com>
2026-02-07 19:40:48 +0100

    chore: initialize spring boot base

---
+++ src/main/java/com/inquilino/perfecto/InquilinoPerfectoBackApplication.java
@@ -0,0 +1,11 @@
+package com.inquilino.perfecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquilinoPerfectoBackApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InquilinoPerfectoBackApplication.class, args);
+	}
+}
